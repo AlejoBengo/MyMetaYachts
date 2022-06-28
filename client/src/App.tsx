a7c23fd00200af87fb23d175df8f2d0710ca45e6
Alexis-Mendez2812 <281212.namaste@gmail.com>
2022-06-28 14:28:01 -0300

    inicial

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Box } from '@mui/material';
+import { Box,Typography } from '@mui/material';
+
 
 function App() {
-   return <Box></Box>;
+   return (<Box>
+   <Typography>Hello</Typography>
+
+   </Box>)
 }
 
 export default App;
